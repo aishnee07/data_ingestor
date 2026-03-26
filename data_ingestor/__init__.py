f026373cb8ec849b38b6992923e94d4be5b763ea
aishnee07 <aishneeporwal@gmail.com>
2026-03-26 09:10:59 +0530

    refactor repo structure to resolve circular imports

--- data_ingestor/__init__.py
+++ data_ingestor/__init__.py
@@ -1,1 +1,7 @@
 # make the directory a package for easy imports of classes and functions from other files in the directory
+from .base import FileIngestor
+from .ingestors import CSVIngestor, JSONIngestor, ParquetIngestor
+from .factory import get_ingestor
+from .context import managed_ingestor
+from .utils import scan_and_ingest, read_multiple_files
+from .decorators import timer, validate_dataframe
